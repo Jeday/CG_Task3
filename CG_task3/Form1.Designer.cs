b1d78dd401b18f334e2b999df6423b101a6e3669
Je_day <je.mouth@gmail.com>
2018-09-29 21:54:55 +0300

    border drawing, icon, borded overlay picture box, border list

--- CG_task3/Form1.Designer.cs
+++ CG_task3/Form1.Designer.cs
@@ -29,32 +29,25 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Painter));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             this.ColorButton = new System.Windows.Forms.Button();
             this.LoadButton = new System.Windows.Forms.Button();
             this.SaveButton = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.BorderPictureBox = new System.Windows.Forms.PictureBox();
             this.ColorBox = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BorderPictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ColorBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(592, 411);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
             // 
             // toolStrip1
             // 
@@ -72,46 +65,12 @@
             // toolStripButton1
             // 
             this.toolStripButton1.CheckOnClick = true;
-            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.Image = global::CG_task3.Properties.Resources.lassoicon;
             this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton1.Name = "toolStripButton1";
-            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Size = new System.Drawing.Size(62, 22);
             this.toolStripButton1.Text = "Border";
             this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton_Click);
-            // 
-            // toolStripButton2
-            // 
-            this.toolStripButton2.CheckOnClick = true;
-            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
-            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton2.Name = "toolStripButton2";
-            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
-            this.toolStripButton2.Text = "Fill";
-            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton_Click);
-            // 
-            // toolStripButton3
-            // 
-            this.toolStripButton3.CheckOnClick = true;
-            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
-            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton3.Name = "toolStripButton3";
-            this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
-            this.toolStripButton3.Text = "Magic Wand";
-            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton_Click);
-            // 
-            // toolStripButton4
-            // 
-            this.toolStripButton4.CheckOnClick = true;
-            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
-            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton4.Name = "toolStripButton4";
-            this.toolStripButton4.Size = new System.Drawing.Size(23, 22);
-            this.toolStripButton4.Text = "Marker";
-            this.toolStripButton4.Click += new System.EventHandler(this.toolStripButton_Click);
             // 
             // ColorButton
             // 
@@ -152,6 +111,21 @@
             // 
             this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
             // 
+            // BorderPictureBox
+            // 
+            this.BorderPictureBox.BackColor = System.Drawing.Color.Transparent;
+            this.BorderPictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BorderPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.BorderPictureBox.Location = new System.Drawing.Point(12, 27);
+            this.BorderPictureBox.Name = "BorderPictureBox";
+            this.BorderPictureBox.Size = new System.Drawing.Size(592, 411);
+            this.BorderPictureBox.TabIndex = 6;
+            this.BorderPictureBox.TabStop = false;
+            this.BorderPictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.BorderPictureBox_Paint);
+            this.BorderPictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.BorderPictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.BorderPictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
             // ColorBox
             // 
             this.ColorBox.Location = new System.Drawing.Point(204, 2);
@@ -160,11 +134,56 @@
             this.ColorBox.TabIndex = 5;
             this.ColorBox.TabStop = false;
             // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.CheckOnClick = true;
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "Fill";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.CheckOnClick = true;
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton3.Text = "Magic Wand";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton_Click);
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.CheckOnClick = true;
+            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton4.Text = "Marker";
+            this.toolStripButton4.Click += new System.EventHandler(this.toolStripButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(592, 411);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
             // Painter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(624, 450);
+            this.Controls.Add(this.BorderPictureBox);
             this.Controls.Add(this.ColorBox);
             this.Controls.Add(this.SaveButton);
             this.Controls.Add(this.LoadButton);
@@ -175,10 +194,11 @@
             this.Name = "Painter";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BorderPictureBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ColorBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -199,6 +219,7 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
         private System.Windows.Forms.PictureBox ColorBox;
+        private System.Windows.Forms.PictureBox BorderPictureBox;
     }
 }
 
--- CG_task3/Form1.cs
+++ CG_task3/Form1.cs
@@ -16,17 +16,27 @@ namespace CG_task3
         private Tools CurrentTool = Tools.None;
         private Color CurrentColor = Color.White;
         private Graphics g;
+        private Graphics borderboxGraphics;
+        private List<Point> border;
+        private bool BorderIsDrawn = false;
+        private Pen BorderPen;
 
         public Painter()
         {
             InitializeComponent();
-            g = pictureBox1.CreateGraphics();
+            
+            
             ColorBox.BackColor = CurrentColor;
+            border = new List<Point>();
+            BorderPen = new Pen(Color.Black, 2);
+            float[] dashValues = {1,1};
+            BorderPen.DashPattern = dashValues;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            borderboxGraphics = BorderPictureBox.CreateGraphics();
+            g = pictureBox1.CreateGraphics();
         }
 
         private void toolStripButton_Click(object sender, EventArgs e)
@@ -36,7 +46,7 @@ namespace CG_task3
                 if (b != button)
                     b.Checked = false;
             }
-            switch (button.Name)
+            switch (button.Text)
             {
                 case "Border":
                     CurrentTool = Tools.BorderMaker;
@@ -89,6 +99,54 @@ namespace CG_task3
         private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
         {
             pictureBox1.Image.Save(saveFileDialog1.FileName);
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (CurrentTool == Tools.BorderMaker)
+                {
+                    border.Clear();
+                    BorderIsDrawn = true;
+                    border.Add(e.Location);
+                    BorderPictureBox.Invalidate();
+                }
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) {
+                if (CurrentTool == Tools.BorderMaker) {
+                    if (BorderIsDrawn && !border.Contains(e.Location))
+                        border.Add(e.Location);
+                    BorderPictureBox.Invalidate();
+                }
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (CurrentTool == Tools.BorderMaker)
+            {
+                BorderIsDrawn = false;
+                BorderPictureBox.Invalidate();
+            }
+        }
+
+        private void BorderPictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            borderboxGraphics = e.Graphics;
+            //borderboxGraphics.Clear(Color.Transparent);
+            if (border.Count > 3)
+            {
+                borderboxGraphics.DrawCurve(BorderPen, border.ToArray());
+                if (!BorderIsDrawn)
+                    borderboxGraphics.DrawLine(BorderPen, border.Last(), border.First());
+                
+                
+            }
         }
     }
 }
