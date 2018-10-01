2285a55fc4d7e1905af1f1132410c9a2cbd15ce7
Evgeny Taktarov <Jeday@users.noreply.github.com>
2018-10-02 02:20:20 +0300

    Add files via upload

--- CG_task3/Form1.cs
+++ CG_task3/Form1.cs
@@ -179,30 +179,16 @@ namespace CG_task3
                             x++;
                     }
 
+                   
 
-                    if (full_border.Count == 0)
-                    {
-                        for (int y =0; y != DrawArea.Height; y++)
-                        {
-                            colored_lines.Add(y, new List<Tuple<int, int>>());
-                            ScanLine(new Point(0,y), new Point(DrawArea.Width, y), c);
-                            
-                            
+                    int top = full_border.Last().Y;
+                    for (int y = full_border.First().Y; y != top; y++) {
+                        List<Point> ps = full_border.Where((Point p) => { return (p.Y == y) && is_not_hill(p); }).ToList();
+                        for (int i = 0; i < ps.Count - 1; i++) {
+                            ScanLine(ps[i], ps[i + 1], c);
                         }
                     }
-                    else
-                    {
-                        int top = full_border.Last().Y;
-                        for (int y = full_border.First().Y; y != top; y++)
-                        {
-                            List<Point> ps = full_border.Where((Point p) => { return (p.Y == y) && is_not_hill(p); }).ToList();
-                            colored_lines.Add(y, new List<Tuple<int, int>>());
-                            for (int i = 0; i < ps.Count - 1; i++)
-                            {
-                                ScanLine(ps[i], ps[i + 1], c);
-                            }
-                        }
-                    }
+
 
                     BorderPictureBox.Invalidate();
 
@@ -217,18 +203,10 @@ namespace CG_task3
         {
             Point Start = new Point(p1.X, p1.Y); 
             p1.X += 1;
-            
-            
-
-            Point start_c = Start;
             while (p1.X < p2.X)
             {
-                
+
                 if (inner_border.Contains(p1)) { // натыкаемся на результат обведения  произведенного выше
-
-                    colored_lines[Start.Y].Add(new Tuple<int, int>(start_c.X,p1.X-1)); // as we are bout to jump, stash for coloring all that before 
-
-
                     var ls = inner_border.Where(p => p.Y == Start.Y).Select((Point p) => p.X).ToList();
                     int i = 0;
                     // skipp to p1 location
@@ -257,13 +235,13 @@ namespace CG_task3
 
                     p1.X = ls[i];
 
-                    start_c = new Point(p1.X + 1, p1.Y);
+
                 }
                 else if (!equal_color(DrawArea.GetPixel(p1.X, p1.Y), c)) // если найденная новая граница
                 {
-                   // DrawArea.SetPixel(p1.X, p1.Y, Color.Red);
-                    magic_border(p1, Start, c, out int x,true); // обводим ее
-                    p1.X = x;  // получаем крайнюю точку 
+                    DrawArea.SetPixel(p1.X, p1.Y, Color.Red);
+                    //magic_border(p1, Start, c, out int x,true); // обводим ее
+                    //p1.X = x;  // получаем крайнюю точку 
 
                 }
                 
@@ -271,7 +249,6 @@ namespace CG_task3
 
 
             }
-            colored_lines[Start.Y].Add(new Tuple<int, int>(start_c.X, p1.X - 1));
 
         }
 
@@ -394,13 +371,13 @@ namespace CG_task3
         }
 
         private void rec_fill(Point p) {
-            if (!inner_border.Contains(p) && !full_border.Contains(p) && !is_colored(p) && p.X >= 0 && p.X <= pictureBox1.Width && p.Y >= 0 && p.Y <= pictureBox1.Height) {
+            if (!full_border.Contains(p) && !is_colored(p) && p.X >= 0 && p.X <= pictureBox1.Width && p.Y >= 0 && p.Y <= pictureBox1.Height) {
                 Point Start = new Point(p.X - 1, p.Y);
                 Point Finish = new Point(p.X + 1, p.Y);
-                while (!inner_border.Contains(p) && !full_border.Contains(Start) && Start.X >= 0)
+                while (!full_border.Contains(Start) && Start.X >= 0)
                     Start.X -= 1;
                 Start.X += 1;
-                while (!inner_border.Contains(p) && !full_border.Contains(Finish)  && Finish.X<pictureBox1.Width)
+                while (!full_border.Contains(Finish)  && Finish.X<pictureBox1.Width)
                     Finish.X += 1;
                 Finish.X -= 1;
                 if(colored_lines.ContainsKey(p.Y))
@@ -589,9 +566,6 @@ namespace CG_task3
         {
             ColorButton_Click(sender, e);
         }
-
-
-
     }
 
     public class PointCompare : IComparer<Point>
